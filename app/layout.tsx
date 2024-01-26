94605fdeea1c3231f4652a3e96ee138f1f4eff24
batihandev <batihandev@gmail.com>
2024-01-26 18:24:46 +0300

    feat(layout.tsx): create a new layout component to be used as the root layout for the application feat(global.css): add global styles for the application feat(onboarding/page.tsx): create a new onboarding page component feat(page.tsx): create a new home page component feat(sign-in/page.tsx): create a new sign-in page component feat(sign-up/page.tsx): create a new sign-up page component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./global.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
@@ -19,24 +19,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <html lang="en">
+      <ClerkProvider>
         <body className={inter.className}>
           <Topbar />
           <main>
             <LeftSidebar />
 
             <section className="main-container">
-              <div className="w-full max-w-4x1">
-                {children}
-              </div>
+              <div className="w-full max-w-4x1">{children}</div>
             </section>
 
             <RightSidebar />
           </main>
           <Bottombar />
         </body>
-      </html>
-    </ClerkProvider>
+      </ClerkProvider>
+    </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
+      <Link className="text-red-400" href="/onboarding">
+        Sa
+      </Link>
     </div>
   );
 }
